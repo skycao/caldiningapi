c6e7a36940e4a8ee616f19525ebd5cc2b1a65855
Sky Cao <cao_sky@yahoo.com>
2013-09-28 16:23:31 -0700

    modified SoupDemo.py

--- SoupDemo.py
+++ SoupDemo.py
@@ -17,6 +17,8 @@ class NutFacts:
         self.iron = Iron
     def __init__(self, NutFactList):
         self.list = NutFactList
+    def __init__(self, NutFactDict):
+        self.dict = NutFactDict
 
 class Item:
     def __init__(self, Name, MealType, NutritionFacts):
@@ -28,4 +30,14 @@ class DiningCommon:
     def __init__(self, Name, ItemList):
         self.name = Name
         self.list = ItemList
+
+def getmenu(Name, MealType):
+    url = "http://services.housing.berkeley.edu/FoodPro/dining/static/DiningMenus.asp?dtCurDate=9/28/2013&strCurLocation=01&strCurLocationName=" + Name.upper()
+    page = urllib2.urlopen(url)
+    soup = BeautifulSoup(page.read())
+    for link in soup.find_all('a'):
+        if 'label.asp?locationNum' in link.get('href'):
+            
+    
+    
 
