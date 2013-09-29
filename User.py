9bcb9add20f1549ab222d0659ffbd6c7fd3ce9aa
rhdsmnd <rhdsmnd@gmail.com>
2013-09-28 17:30:55 -0700

    Update User.py

--- User.py
+++ User.py
@@ -7,9 +7,10 @@ class User(object):
 class Diet(object):
     
     
-    def __init__(calorie_limit, protein_limit, carb_limit, fat_limit, bulking):
+    def __init__(calorie_limit, protein_limit, carb_limit, fat_limit, bulking, meat_vege_veg):
       self.bulking = bulking
       self.calorie_limit = calorie_limit
       self.protein_limit = protein_limit
       self.carb_limit = carb_limit
       self.fat_limit = fat_limit
+      self.food_type = meat_vege_veg
