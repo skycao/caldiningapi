b4d0ba1f31d286e4a3d2dccb703ad635a5856b96
Daniel Radding <dradding@berkeley.edu>
2013-09-28 18:57:48 -0700

    record.py finished

--- record.py
+++ record.py
@@ -1,1 +1,12 @@
 """Write into from Soup Demo to file"""
+def write(DC):
+    ofile = csv.writer(open('test.csv', "wb"))
+    ofile.writerow = (["name","vegan","calories","totalfat","satfat","transfat","cholesterol","sodium","totalcarbs","dietfib","sugar","protein","vitc","calcium","iron"])
+    row = []
+    for x in DC.list:
+        row.append(x.name.toString())
+        row.append(x.veg.toString())
+        for y in x.nutfacts:
+            row.append(y.toString()):
+        ofile.writerow(row)
+    ofile.close()
