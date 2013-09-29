a27a454115115ea49ee6275f450088308d813980
Daniel Radding <dradding@berkeley.edu>
2013-09-28 19:12:10 -0700

    fix record.py

--- record.py
+++ record.py
@@ -2,8 +2,8 @@
 def write(DC):
     ofile = csv.writer(open('test.csv', "wb"))
     ofile.writerow = (["name","vegan","calories","totalfat","satfat","transfat","cholesterol","sodium","totalcarbs","dietfib","sugar","protein","vitc","calcium","iron"])
-    row = []
     for x in DC.list:
+        row = []
         row.append(x.name.toString())
         row.append(x.veg.toString())
         for y in x.nutfacts:
